fc85091d3e2a538e80c69e378321d71320a3ed7e
Tarun Jangra <tarunjangra043@gmail.com>
2025-05-30 15:28:16 +0530

    updated

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -15,7 +15,6 @@ import info_icon from "./info_icon.svg";
 import upload_icon from "./upload_icon.png";
 import stripe_logo from "./stripe_logo.png";
 import razorpay_logo from "./razorpay_logo.png";
-import favicon from "./favicon.svg";
 import doc1 from "./doc1.png";
 import doc2 from "./doc2.png";
 import doc3 from "./doc3.png";
@@ -57,7 +56,6 @@ export const assets = {
   upload_icon,
   stripe_logo,
   razorpay_logo,
-  favicon,
 };
 
 export const specialityData = [
